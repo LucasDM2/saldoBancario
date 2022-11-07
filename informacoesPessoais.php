ff88f664254d293ace6280cbfc2df90436370a47
Lucas Dellandréa <lucas.dellandrea@conectra.com.br>
2022-11-07 16:58:39 -0300

    feat: implementando formulário.

--- informacoesPessoais.php
+++ informacoesPessoais.php
@@ -5,37 +5,110 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dados bancários</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="js/jquery-3.6.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.mask.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+        $("#salario").mask("000000000.00", {reverse: true})
+        $("#divida").mask("000000000.00", {reverse: true})
+        })
+    </script>
 </head>
 <body>
-<?php
-    $nome = $_POST["nome"];
-    $telefone = $_POST["telefone"];
-        
-?>
-    <form action="saldo.php" method="post">
-        <h1>Dados bancários</h1>
-<?php
-    echo "Olá $nome, informe-nos algumas credenciais."
-?>
-        <br>
-        <br>
-        <br>
-        Saldo da conta:<br> 
-        <input class="input" type="number" value="" name="saldo"><br>
-        <br>
-        Nome do banco:<br> 
-        <input class="input" type="text" name="banco"><br>
-        <br>
-        Valor em dívidas:<br> 
-        <input class="input" type="number" value="" name="dívidas"><br>
-        <br>
-        <input type="submit" value="Enviar dados" value="">
-    </form> 
+    <header>
+        <h1>Dados bancários do usuário.</h1>
+    </header>
+    <h2><?php echo $nome = $_POST["nome"]," ", $sobrenome = $_POST["sobrenome"] ?>, informe-nos algumas credenciais:</h2>
+    <section>       
+        <form action="saldo.php" method="post">
+            <div>   
+                Saldo da conta: 
+                <input autocomplete="off" class="input" placeholder="Digite seu saldo." type="text" value="" name="salario" id="salario" required>
+            </div>
+            <div>
+                Banco:
+                <select required name="banco" autocomplete="off" id="banco">
+                    <option value="" disabled selected>Selecione um banco:</option>
+                    <option value="banco 'Caixa Econômica'"> Caixa Econômica</option>
+                    <option value="'banco do Brasil'"> Banco do Brasil </option>
+                    <option value="banco 'Itaú'"> Itaú </option>
+                    <option value="banco 'Bradesco'"> Bradesco</option>
+                    <option value="banco 'Santander'"> Santander</option>
+                    <option value="banco 'Inter'"> Banco Inter</option>
+                    <option value="banco 'HSBC'"> HSBC</option>
+                    <option value="banco 'BNDES'"> BNDES</option>
+                    <option value="banco 'Banrisul'"> Banrisul</option>
+                    <option value="banco 'Sicredi'"> Sicredi</option>
+                </select>
+            </div>
+            <div>
+                Valor em dívidas:
+                <input autocomplete="off" class="input" placeholder="Digite o valor de dívidas." type="text" value="" name="divida" id="divida" required>
+            </div>
+            <div>
+                <input class="botao" type="submit" value="Enviar dados" value="">
+            </div>
+        </form> 
+    </section>
 </body>
 </html>
 
 <style>
+    * {
+        padding: 0;
+        margin: 0;
+        justify-content: center;
+        align-items: center;
+    }
+
     .input {
+        width: 300px;
+    }
+
+    select {
+        background-color: white;
+    }
+
+    header {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 20px;
+        background-color: cornflowerblue;
+        color: snow;
+        font-family: Cambria, Arial;
+    }
+
+    section {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 80vh;
+    }
+    
+    form {
+        font-family: arial;
+    }
+
+    form input, form textarea, form select {
+        padding: 20px;
+        width: 300px;
+        border: 1px solid cornflowerblue;
+        border-radius: 20px;
+    }
+
+    .botao {
+        background-color: cornflowerblue;
+        color: snow;
         width: 200px;
+        cursor: pointer;
+    }
+
+    form div {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 30px;
     }
 </style>
--- saldo.php
+++ saldo.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,20 +7,62 @@
     <title>Saldo</title>
 </head>
 <body>
+    <header>
+        <h1>Saldo após quitação das dívidas. <?php $resultado ?></h1>
+    </header>
+    <section>
     <?php
-        $saldo = $_POST["saldo"];
-        $dívidas = $_POST["dívidas"];
-        $resultado = ($saldo - $dívidas);
+        $salario = $_POST["salario"];
+        $divida = $_POST["divida"];
+        $resultado = ($salario - $divida);
+        $banco = $_POST["banco"];
+        $nome = $_POST["nome"];
 
         if ($resultado > 0) {
-            echo "Ainda restará dinheiro em sua conta, totalizando $resultado";
+            echo "Após quitar sua dívida com o $banco, ainda restará dinheiro em sua conta, totalizando R$";
+            echo number_format($resultado,2,",","."), "."; 
         }
         else if (($resultado) == 0) {
-            echo "Após quitar sua dívida, não restará nada em sua conta";
+            echo "Após quitar sua dívida com o $banco, não restará nada em sua conta.";
         }
         else {
-            echo "O saldo da sua conta ficará negativo, resultando numa dívida de $resultado";
+            echo "Após quitar sua dívida com o $banco, o saldo da sua conta ficará negativo, resultando numa dívida de R$";
+            echo number_format($resultado,2,",","."), "."; 
         }
+        
     ?>
+    </section>
 </body>
 </html>
+
+<style>
+    body {
+        padding: 0;
+        margin: 0;
+        justify-content: center;
+        align-items: center;
+        background-color: cornflowerblue;
+    }
+
+    header {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 20px;
+        background-color: cornflowerblue;
+        color: snow;
+        font-family: Cambria, Arial;
+    }
+
+    section{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 20px;
+        border: 300px solid cornflowerblue;
+        border-radius: 2px;
+        font-size: 28px;
+        color: snow;
+    }
+
+</style>
